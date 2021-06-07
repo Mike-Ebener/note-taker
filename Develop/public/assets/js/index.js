72fd7fd0d4fdfb03b536e410c171cf3932f73e55
ME <mebener44@gmail.com>
2021-06-07 11:48:55 -0500

    updated with improved routes

--- Develop/public/assets/js/index.js
+++ Develop/public/assets/js/index.js
@@ -3,6 +3,7 @@ let noteText;
 let saveNoteBtn;
 let newNoteBtn;
 let noteList;
+// const db = require('../../../db/db.json');
 
 if (window.location.pathname === '/notes') {
   noteTitle = document.querySelector('.note-title');
--- server.js
+++ server.js
@@ -1,15 +1,37 @@
 const express = require('express');
 const { notes } = require('./Develop/db/db.json');
 const PORT = process.env.PORT || 3001;
+const path = require('path');
 const app = express();
+//added fs
+const fs = require('fs');
+
+app.use(express.static('public'));
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
 
 app.get('/api/notes', (req, res) => {
     res.json(notes);
   });
 
+  app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, './Develop/public/index.html'));
+  });
+
+  app.get('/notes', (req, res) => {
+    res.sendFile(path.join(__dirname, './Develop/public/notes.html'));
+    //console.log('test');
+});
+
+//catch all for
+  app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, './Develop/public/index.html'));
+  });
+
 
   app.post('/api/notes', (req, res) => {  
       // req.body is where our incoming content will be
+      
     console.log(req.body);
     res.json(req.body);});
 
